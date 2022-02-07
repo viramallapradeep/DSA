487234d9d065a25483416fcec535270f5e047c9e
pradeep <pradeep.viramalla@gmail.com>
2022-02-07 23:39:30 +0530

    added DSA

---
+++ src/arrayOne/LongestSubArrayWithSumZero.java
@@ -0,0 +1,13 @@
+package arrayOne;
+
+public class LongestSubArrayWithSumZero {
+	
+	
+		public static void main(String[] args) {
+		
+		
+		
+		
+	}
+}
+
---
+++ src/linkedlist/NthNodeFromEnd.java
@@ -0,0 +1,69 @@
+package linkedlist;
+
+public class NthNodeFromEnd {
+	
+	
+	
+	public static   void getNnthNodeFromEnd(Node head,int val) {
+			
+			
+		Node curnt = head;
+		int count = 0; 
+		
+		while (curnt!=null) {
+			count++;
+			curnt = curnt.getNextNode();
+		}
+			
+			Node tempNode = head;
+			
+			int temCount = 0;
+			
+				while (tempNode!=null) {
+					
+					temCount++;
+					if (temCount == (count - val)+1) {
+						System.out.println(tempNode.getData());
+						break;
+					}
+					
+					tempNode = tempNode.getNextNode();
+			}
+			
+	}
+	
+	static int i = 0;
+	
+	public static   void getNnthNodeFromEndByRec(Node head,int n) {
+		
+		  
+		    if (head == null) {
+		        return;
+		    }
+		    
+		    getNnthNodeFromEndByRec(head.getNextNode(), n);
+		  
+		    if (++i == n)
+		        System.out.print(head.getData());
+	}
+	
+
+	
+	
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+
+		list.addAfterHead(1);
+		list.addAfterHead(2);
+		list.addAfterHead(3);
+		list.addAfterHead(4);
+		list.addAfterHead(5);
+		
+		
+		getNnthNodeFromEndByRec(list.head, 2);
+		
+		
+	}
+
+}
